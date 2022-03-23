61bfb1007f9f0ddb5488c4fcd2647ff9cf9a42d5
Gabriel Couto <Gabriel_Couto@gap.com>
2022-03-22 22:15:21 -0300

    add spa v1

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("click", (event) => {
-  if (event.target.nodeName === "A") {
-    event.preventDefault();
-    window.location.href += "about";
-  }
-});
---
+++ static/script.js
@@ -0,0 +1,39 @@
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => `<a href="/about">about page</a>` },
+    { path: "/about", view: () => `<a href="/">home page</a>` },
+  ];
+
+  let match = routes.find((route) => {
+    return window.location.pathname === route.path;
+  });
+
+  if (!match) {
+    match = {
+      route: routes[0],
+    };
+  }
+  document.querySelector("#app").innerHTML = match.route.view();
+};
+
+const navigate = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (event) => {
+    if (event.target.nodeName === "A") {
+      console.log("here");
+      event.preventDefault();
+      navigate(event.target.href);
+    }
+  });
+
+  router();
+});
